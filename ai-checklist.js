77bdf939e9033c24006e098430aba260299bc303
CraigAUT <143149479+CraigAUT@users.noreply.github.com>
2023-08-25 14:21:39 +1200

    Add files via upload

---
+++ ai-checklist.js
@@ -0,0 +1,5 @@
+
+document.getElementById("ai-checklist").addEventListener("submit", function(event){
+    event.preventDefault(); // Prevents form submission
+    alert("Thank you for considering AI ethics and effectiveness.");
+});
